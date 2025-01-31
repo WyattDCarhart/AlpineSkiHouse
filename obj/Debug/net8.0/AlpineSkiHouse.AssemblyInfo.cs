ee7b713457dedda5fbe7a637f49114ba1dd87a8f
WyattDCarhart <harmony@sc.edu>
2025-01-31 11:27:55 -0500

    readme

--- obj/Debug/net8.0/AlpineSkiHouse.AssemblyInfo.cs
+++ obj/Debug/net8.0/AlpineSkiHouse.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AlpineSkiHouse")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2d4f40eab42049cce84cb9b9c8912aae002c8133")]
 [assembly: System.Reflection.AssemblyProductAttribute("AlpineSkiHouse")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AlpineSkiHouse")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
